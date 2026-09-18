784bcb7e10d4cb26c37978ff6c19b5ef981deb63
GAVUTHAM D <gavuthaman27@gmail.com>
2026-09-18 07:32:48 +0530

    Time: 57 ms (8.46%), Space: 47.8 MB (49.71%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -2,10 +2,9 @@ class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
         List <Integer> al = new ArrayList<>();
         int i = num.length-1;
-        while(i>=0 || k>0){
+        while(i>=0 || k!=0){
             if(i>=0){
-                k+=num[i];
-                i--;
+                k+=num[i--];
             }
             al.add(0,k%10);
             k/=10;
